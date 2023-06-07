3e081954982d2d8f567300fc6e6930c0f6465f42
sparshgahlot1 <sparshgahlot0398@gmail.com>
2023-06-07 20:20:40 +0530

    Gallery.css ready

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -40,7 +40,7 @@ const Gallery = () => {
           ))}
         </div>
 
-      <div className='app__gallery-images_arrow'>
+      <div className='app__gallery-images_arrows'>
         <BsArrowLeftShort className='gallery__arrow-icon' onClick={ () => scroll('left')}/>
         <BsArrowRightShort className='gallery__arrow-icon' onClick={ () => scroll('right')}/>
       </div>
